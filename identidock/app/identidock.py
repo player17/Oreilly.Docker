45e73377d261067efb987c3b52a2772d8df2f91c
reandh <reandh@yandex.ru>
2019-05-23 16:39:52 +0500

    refactoring dir

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!\n'
+    return 'Php Strom Start!\n'
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True,use_reloader=True, host='0.0.0.0')
