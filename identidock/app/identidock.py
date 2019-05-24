241bc3f8bafd2b3cc2091fc8adf46c1fe2854bc6
reandh <reandh@yandex.ru>
2019-05-24 22:42:42 +0500

    add cmd.sh  // Работает посредственно

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -2,10 +2,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Php Strom Start 555!\n'
+    return 'Php Strom Start!\n'
 
 if __name__ == '__main__':
-    app.run(port=9090,debug=True,use_reloader=True, host='0.0.0.0')
+    app.run(port=5000,debug=True,use_reloader=True, host='0.0.0.0')
