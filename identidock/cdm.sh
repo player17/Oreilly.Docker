1a6a4f2a45f73bf78fdabbbe17728e39b962426b
reandh <reandh@yandex.ru>
2019-05-25 06:22:41 +0500

    test WARNING

--- identidock/cdm.sh
+++ identidock/cdm.sh
@@ -4,7 +4,6 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   exec python "identidock.py"
-  chmod 644 identidock.py
 else
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
